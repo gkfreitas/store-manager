ff3fc174ff1180de17fc9e91265c58d2c79ad5d0
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-06-01 17:57:04 -0300

    fix5

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
+  host: process.env.MYSQL_HOSTNAME,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
